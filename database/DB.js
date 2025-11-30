fde6ae815fb9c3a82e2fc4aaa9f6b901128ee156
kanika <kanikajain1514@gmail.com>
2025-11-30 10:22:36 +0530

    hey

--- database/DB.js
+++ database/DB.js
@@ -1,8 +1,5 @@
-const mongooose= require('mongoose');
-// mongooose.connect('mongodb://localhost:27017/').then(()=>{
-//     console.log("database is connected");
-// })
-
+const mongoose = require('mongoose');
  mongoose.connect('mongodb+srv://mernuser:supperpassword@cluster0.cqczybv.mongodb.net/mydatabase').then(()=>{
-    console.log("database connected successfully");
+     console.log("database connected successfully");
  });
+
